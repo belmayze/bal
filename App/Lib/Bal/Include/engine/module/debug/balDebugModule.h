49faf2eb608db683a45e151236297c17b07013ca
belmayze <belmayze@hotmail.co.jp>
2021-05-24 10:09:23 +0900

    デバッグモジュール

---
+++ App/Lib/Bal/Include/engine/module/debug/balDebugModule.h
@@ -0,0 +1,58 @@
+﻿/*!
+ * @file   balDebugModule.h
+ * @brief  デバッグモジュール
+ * @author belmayze
+ *
+ * Copyright (c) 2021 belmayze. All rights reserved.
+ */
+#pragma once
+// bal
+#include <engine/module/balModuleInstance.h>
+
+// ----------------------------------------------------------------------------
+namespace bal::mod::debug {
+
+class Module : public ModuleInstance<Module>
+{
+public:
+    /*!
+     * コンストラクター
+     */
+    Module();
+
+    /*!
+     * デストラクター
+     */
+    virtual ~Module();
+
+    /*!
+     * カスタムモジュールを登録する
+     * @param[in] p_custom_module カスタムモジュール
+     */
+    virtual void setCustomModule(std::unique_ptr<mod::ICustomModule>&& p_custom_module) override;
+
+    /*!
+     * モジュールの初期化を行う関数です
+     * @param[in] arg 初期化引数
+     */
+    virtual void initialize(const InitializeArg& arg) override;
+
+    /*!
+     * モジュールの終了処理を行う関数です
+     */
+    virtual void finalize() override;
+
+    /*!
+     * モジュールの更新を行う関数です
+     * @param[in] arg 更新引数
+     */
+    virtual void onUpdate(const FrameworkCallback::UpdateArg& arg) override;
+
+    /*!
+     * 描画を行う関数です
+     * @param[in] arg 描画引数
+     */
+    virtual void onDraw(const FrameworkCallback::DrawArg& arg) override {}
+};
+
+}
---
+++ App/Lib/Bal/Source/engine/module/debug/balDebugModule.cpp
@@ -0,0 +1,47 @@
+﻿/*!
+ * @file   balDebugModule.cpp
+ * @brief  
+ * @author belmayze
+ *
+ * Copyright (c) 2021 belmayze. All rights reserved.
+ */
+// bal
+#include <engine/module/debug/balDebugModule.h>
+
+namespace bal::mod::debug {
+
+// ----------------------------------------------------------------------------
+
+Module::Module() {}
+
+// ----------------------------------------------------------------------------
+
+Module::~Module() {}
+
+// ----------------------------------------------------------------------------
+
+void Module::setCustomModule(std::unique_ptr<mod::ICustomModule>&& p_custom_module)
+{
+}
+
+// ----------------------------------------------------------------------------
+
+void Module::initialize(const InitializeArg& arg)
+{
+}
+
+// ----------------------------------------------------------------------------
+
+void Module::finalize()
+{
+}
+
+// ----------------------------------------------------------------------------
+
+void Module::onUpdate(const FrameworkCallback::UpdateArg& arg)
+{
+}
+
+// ----------------------------------------------------------------------------
+
+}
