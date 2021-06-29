3303d3689363cab4172b4d5a43e48c802f6535c0
belmayze <belmayze@hotmail.co.jp>
2021-06-29 16:52:11 +0900

    テクスチャーコンバートフォーマット追加

--- App/Lib/Bal/Tool/Build/TextureConverter/TextureConverter/Main.cpp
+++ App/Lib/Bal/Tool/Build/TextureConverter/TextureConverter/Main.cpp
@@ -96,6 +96,28 @@ public:
         // BC7: 8bpp（4チャンネル） -> BC3の後継
         else if (format_str.isEquals("bc7_srgb"))  { compress_format = DXGI_FORMAT_BC7_UNORM_SRGB; }
         else if (format_str.isEquals("bc7_unorm")) { compress_format = DXGI_FORMAT_BC7_UNORM; }
+        // float
+        else if (format_str.isEquals("r32g32b32a32_float")) { compress_format = DXGI_FORMAT_R32G32B32A32_FLOAT; }
+        else if (format_str.isEquals("r32g32b32_float"))    { compress_format = DXGI_FORMAT_R32G32B32_FLOAT; }
+        else if (format_str.isEquals("r32g32_float"))       { compress_format = DXGI_FORMAT_R32G32_FLOAT; }
+        else if (format_str.isEquals("r32_float"))          { compress_format = DXGI_FORMAT_R32_FLOAT; }
+        else if (format_str.isEquals("r16g16b16a16_float")) { compress_format = DXGI_FORMAT_R16G16B16A16_FLOAT; }
+        else if (format_str.isEquals("r16g16_float"))       { compress_format = DXGI_FORMAT_R16G16_FLOAT; }
+        else if (format_str.isEquals("r11g11b10_float"))    { compress_format = DXGI_FORMAT_R11G11B10_FLOAT; }
+        // RGBA
+        else if (format_str.isEquals("r8g8b8a8_unorm")) { compress_format = DXGI_FORMAT_R8G8B8A8_UNORM; }
+        else if (format_str.isEquals("r8g8b8a8_srgb"))  { compress_format = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB; }
+        // BGRA
+        else if (format_str.isEquals("b5g5r5a1_unorm")) { compress_format = DXGI_FORMAT_B5G5R5A1_UNORM; }
+        else if (format_str.isEquals("b4g4r4a4_unorm")) { compress_format = DXGI_FORMAT_B4G4R4A4_UNORM; }
+        // RGB
+        else if (format_str.isEquals("r5g6b5_unorm")) { compress_format = DXGI_FORMAT_B5G6R5_UNORM; }
+        // RG
+        else if (format_str.isEquals("r8g8_snorm")) { compress_format = DXGI_FORMAT_R8G8_SNORM; }
+        else if (format_str.isEquals("r8g8_unorm")) { compress_format = DXGI_FORMAT_R8G8_UNORM; }
+        // R
+        else if (format_str.isEquals("r8_snorm")) { compress_format = DXGI_FORMAT_R8_SNORM; }
+        else if (format_str.isEquals("r8_unorm")) { compress_format = DXGI_FORMAT_R8_UNORM; }
 
         bool success = convert_(input_filepath, output_filepath, compress_format);
 
@@ -152,33 +174,69 @@ private:
             return false;
         }
 
-        // 圧縮処理
-        DirectX::ScratchImage compressed_image;
-        hr = DirectX::Compress(
-            raw_image.GetImages(),
-            raw_image.GetImageCount(),
-            raw_image.GetMetadata(),
-            format,
-            DirectX::TEX_COMPRESS_DEFAULT,
-            0.f,
-            compressed_image
-        );
-        if (FAILED(hr))
+        // 同じフォーマットならそのまま書き出す
+        if (raw_image.GetImages()->format == format)
         {
-            return false;
+            // 書き出し
+            hr = DirectX::SaveToDDSFile(
+                raw_image.GetImages(),
+                raw_image.GetImageCount(),
+                raw_image.GetMetadata(),
+                DirectX::DDS_FLAGS_NONE,
+                p_output_filepath.get()
+            );
+            if (FAILED(hr))
+            {
+                return false;
+            }
         }
-
-        // 書き出し
-        hr = DirectX::SaveToDDSFile(
-            compressed_image.GetImages(),
-            compressed_image.GetImageCount(),
-            compressed_image.GetMetadata(),
-            DirectX::DDS_FLAGS_NONE,
-            p_output_filepath.get()
-        );
-        if (FAILED(hr))
+        else
         {
-            return false;
+            // 圧縮処理
+            DirectX::ScratchImage converted_image;
+            if (DirectX::IsCompressed(format))
+            {
+                hr = DirectX::Compress(
+                    raw_image.GetImages(),
+                    raw_image.GetImageCount(),
+                    raw_image.GetMetadata(),
+                    format,
+                    DirectX::TEX_COMPRESS_DEFAULT,
+                    0.f,
+                    converted_image
+                );
+                if (FAILED(hr))
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                hr = DirectX::Convert(
+                    *raw_image.GetImages(),
+                    format,
+                    DirectX::TEX_FILTER_CUBIC,
+                    0.f,
+                    converted_image
+                );
+                if (FAILED(hr))
+                {
+                    return false;
+                }
+            }
+
+            // 書き出し
+            hr = DirectX::SaveToDDSFile(
+                converted_image.GetImages(),
+                converted_image.GetImageCount(),
+                converted_image.GetMetadata(),
+                DirectX::DDS_FLAGS_NONE,
+                p_output_filepath.get()
+            );
+            if (FAILED(hr))
+            {
+                return false;
+            }
         }
 
         DirectX::SetWICFactory(nullptr);
